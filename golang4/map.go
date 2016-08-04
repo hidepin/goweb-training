c8da792bfc523b7929fd6738aef1b28addc20091
Hidetoshi Imai <hidepin@gmail.com>
2016-08-04 22:23:40 +0900

    update map.go

--- golang4/map.go
+++ golang4/map.go
@@ -11,4 +11,25 @@ func main() {
 	for k, v := range ages {
 		fmt.Printf("%s = %d\n", k, v)
 	}
+
+	ages["hoge"] = 123
+	fmt.Println(ages["hoge"])
+
+	for k, v := range ages {
+		fmt.Printf("%s = %d\n", k, v)
+	}
+
+	delete(ages, "hoge")
+
+	for k, v := range ages {
+		fmt.Printf("%s = %d\n", k, v)
+	}
+
+	fmt.Printf("%s = %d\n", "bob", ages["bob"])
+	ages["bob"] = ages["bob"] + 1
+	fmt.Printf("%s = %d\n", "bob", ages["bob"])
+	ages["bob"] += 1
+	fmt.Printf("%s = %d\n", "bob", ages["bob"])
+	ages["bob"]++
+	fmt.Printf("%s = %d\n", "bob", ages["bob"])
 }
