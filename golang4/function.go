d982da869841092df4bbad12e726dd70c4a7c30e
Hidetoshi Imai <hidepin@gmail.com>
2016-08-05 18:12:50 +0900

    add html visit.

--- golang4/function.go
+++ golang4/function.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
+
+	"golang.org/x/net/html"
 )
 
 func hypot(x, y float64) float64 {
@@ -14,6 +17,20 @@ func sub(x, y int) (z int)   { z = x + y; return }
 func first(x int, _ int) int { return x }
 func zero(int, int) int      { return 0 }
 
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
+
 func main() {
 	fmt.Println(hypot(3, 4))
 
@@ -21,4 +38,13 @@ func main() {
 	fmt.Printf("%T\n", sub)
 	fmt.Printf("%T\n", first)
 	fmt.Printf("%T\n", zero)
+
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinkgs1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
 }
