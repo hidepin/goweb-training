f7ec868712e8b2fccc0e07704d6d795d88f5eb0a
Hidetoshi Imai <hidepin@gmail.com>
2016-08-05 18:19:54 +0900

    add outline function.

--- golang4/function.go
+++ golang4/function.go
@@ -31,6 +31,16 @@ func visit(links []string, n *html.Node) []string {
 	return links
 }
 
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data)
+		fmt.Println(stack)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
+}
+
 func main() {
 	fmt.Println(hypot(3, 4))
 
@@ -47,4 +57,6 @@ func main() {
 	for _, link := range visit(nil, doc) {
 		fmt.Println(link)
 	}
+
+	outline(nil, doc)
 }
