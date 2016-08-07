058c6011378077a49650d7ad81b3dc186ae3a18f
Hidetoshi Imai <hidepin@gmail.com>
2016-08-07 20:28:55 +0900

    add findlinks

--- golang4/function.go
+++ golang4/function.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"net/http"
 	"os"
 
 	"golang.org/x/net/html"
@@ -41,6 +42,23 @@ func outline(stack []string, n *html.Node) {
 	}
 }
 
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return visit(nil, doc), nil
+}
+
 func main() {
 	fmt.Println(hypot(3, 4))
 
@@ -59,4 +77,15 @@ func main() {
 	}
 
 	outline(nil, doc)
+
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlink2: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
 }
