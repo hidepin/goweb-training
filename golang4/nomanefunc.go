ebcb779595a8b65485915b4a8adcbb2e53579140
Hidetoshi Imai <hidepin@gmail.com>
2016-08-13 07:53:44 +0900

    add squares.

--- golang4/nomanefunc.go
+++ golang4/nomanefunc.go
@@ -5,6 +5,20 @@ import (
 	"strings"
 )
 
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+
 func main() {
 	fmt.Println(strings.Map(func(r rune) rune { return r + 1 }, "HAL-9000"))
+
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
 }
