90fe94c0f45f387471b8b6209f21ccbc65f3fe80
Hidetoshi Imai <hidepin@gmail.com>
2016-08-14 07:31:30 +0900

    add toposort.

--- golang4/nomanefunc.go
+++ golang4/nomanefunc.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
@@ -13,6 +14,49 @@ func squares() func() int {
 	}
 }
 
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discreate math"},
+	"databases":             {"data structures"},
+	"discreate math":        {"intro to programming"},
+	"formal languages":      {"discreate math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
+}
+
 func main() {
 	fmt.Println(strings.Map(func(r rune) rune { return r + 1 }, "HAL-9000"))
 
@@ -21,4 +65,8 @@ func main() {
 	fmt.Println(f())
 	fmt.Println(f())
 	fmt.Println(f())
+
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
 }
